f6b78af9566b2bf16ee27709d52b40c21aa757c7
Dakoda Wogan <d.dakoda@yahoo.com>
2015-05-20 19:42:01 -0500

    adding .sql

--- migrations.sql
+++ migrations.sql
@@ -2,3 +2,22 @@ psql
 
 CREATE DATABASE box;
 \c box
+
+CREATE TABLE purchases (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  product INTEGER,
+  price VARCHAR(255)
+);
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  product_number INTEGER,
+  is_paid BOOLEAN
+);
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  purchase_id INTEGER,
+  item_id INTEGER
+);
